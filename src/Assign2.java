a6d57fe0c6267a9279f45ac376cc1b911320207c
AkshayO3 <akshaybhandari020@gmail.com>
2023-09-27 18:05:24 +0530

    2-3

--- src/Assign2.java
+++ src/Assign2.java
@@ -10,11 +10,37 @@ public class Assign2 {
 //        else
 //            System.out.println("Odd");
 
-        System.out.println("Enter the temperature");
-        int temp = sc.nextInt();
-        if(temp>100)
-            System.out.println("You have fever");
-        else
-            System.out.println("You are healthy");
+//        System.out.println("Enter the temperature");
+//        int temp = sc.nextInt();
+//        if(temp>100)
+//            System.out.println("You have fever");
+//        else
+//            System.out.println("You are healthy");
+
+        int w = sc.nextInt();
+        switch(w){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+        }
     }
 }
