659be28061e70237fd80031b991b67091b8798ae
AkshayO3 <akshaybhandari020@gmail.com>
2023-09-27 17:57:06 +0530

    Third Problem

--- src/Assign1.java
+++ src/Assign1.java
@@ -9,8 +9,16 @@ public class Assign1 {
 //        double avg=(a+b+c)/3.0;
 //        System.out.println("Average of three numbers is "+avg);
 
-        int size = sc.nextInt();
-        int area = size*size;
-        System.out.println("Area of square is "+area);
+//        int size = sc.nextInt();
+//        int area = size*size;
+//        System.out.println("Area of square is "+area);
+
+        float pencil = sc.nextFloat();
+        float eraser = sc.nextFloat();
+        float pen = sc.nextFloat();
+        float total = pencil+eraser+pen;
+        System.out.println("Total cost is "+total);
+        float totalTax = total + (float) (total*0.18);
+        System.out.println("Total cost with tax is "+totalTax);
     }
 }
