857b3163c0fc039cf9e4a96e340604e4e5a5486d
AkshayO3 <akshaybhandari020@gmail.com>
2023-09-27 18:03:23 +0530

    2-2

--- src/Assign2.java
+++ src/Assign2.java
@@ -2,11 +2,19 @@ import java.util.Scanner;
 public class Assign2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
-        int a = sc.nextInt();
-        if(a%2==0)
-            System.out.println("Even");
+
+//        System.out.println("Enter the number");
+//        int a = sc.nextInt();
+//        if(a%2==0)
+//            System.out.println("Even");
+//        else
+//            System.out.println("Odd");
+
+        System.out.println("Enter the temperature");
+        int temp = sc.nextInt();
+        if(temp>100)
+            System.out.println("You have fever");
         else
-            System.out.println("Odd");
+            System.out.println("You are healthy");
     }
 }
