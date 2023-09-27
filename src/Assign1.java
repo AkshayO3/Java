36911dbea86de26776dc11451d335e8392213d3f
AkshayO3 <akshaybhandari020@gmail.com>
2023-09-27 17:54:18 +0530

    Second Problem

--- src/Assign1.java
+++ src/Assign1.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 public class Assign1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
-        double avg=(a+b+c)/3.0;
-        System.out.println("Average of three numbers is "+avg);
+
+//        int a = sc.nextInt();
+//        int b=sc.nextInt();
+//        int c=sc.nextInt();
+//        double avg=(a+b+c)/3.0;
+//        System.out.println("Average of three numbers is "+avg);
+
+        int size = sc.nextInt();
+        int area = size*size;
+        System.out.println("Area of square is "+area);
     }
 }
