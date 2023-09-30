0c294cae6693e76605cd0e0391f02db1d00bbd57
AkshayO3 <akshaybhandari020@gmail.com>
2023-09-30 09:13:31 +0530

    4-4

--- src/Assign4.java
+++ src/Assign4.java
@@ -28,9 +28,20 @@ public class Assign4 {
 
 
 // Inverted half-pyramid with number patterns
-        for(int i=5;i>0;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print(j+" ");
+//        for(int i=5;i>0;i--){
+//            for(int j=1;j<=i;j++){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//        }
+
+
+// Floyd's triangle
+        int x=0;
+        for(int i=0;i<=5;i++){
+            for(int j=0;j<i;j++){
+                x++;
+                System.out.print(x+"   ");
             }
             System.out.println();
         }
