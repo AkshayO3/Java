d84137fc987899c61cdd3f61719374482e5bf89c
AkshayO3 <akshaybhandari020@gmail.com>
2023-09-30 09:10:58 +0530

    4-1

--- src/Assign4.java
+++ src/Assign4.java
@@ -4,15 +4,27 @@ public class Assign4 {
         Scanner sc = new Scanner(System.in);
 
 // Hollow Rectangle
-        for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                if(i==0 || i==4 || j==0 || j==4){
-                    System.out.print("*");
-                }
-                else System.out.print(" ");
-            }
-            System.out.println();
+//        for(int i=0;i<5;i++){
+//            for(int j=0;j<5;j++){
+//                if(i==0 || i==4 || j==0 || j==4){
+//                    System.out.print("*");
+//                }
+//                else System.out.print(" ");
+//            }
+//            System.out.println();
+//        }
+
+
+// Inverted and Rotated half pyramid
+    for(int i=1;i<5;i++){
+        for(int j=4-i;j>0;j--){
+            System.out.print(" ");
         }
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 
 
 
