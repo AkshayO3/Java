49f3f4010085d0d0ef94b0fc7e9aa9de14a00a4c
AkshayO3 <akshaybhandari020@gmail.com>
2023-09-30 09:20:36 +0530

    4-7

--- src/Assign4.java
+++ src/Assign4.java
@@ -58,17 +58,29 @@ public class Assign4 {
 
 
 // Butterfly Pattern
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j <= i; j++) System.out.print("*");
-            for (int j = 6 - 2 * i; j > 0; j--) System.out.print(" ");
-            for (int j = 0; j <= i; j++) System.out.print("*");
-            System.out.println();
-        }
-        for(int i=0;i<4;i++){
-            for(int j=4-i;j>0;j--) System.out.print("*");
-            for(int j=1;j<=2*i;j++) System.out.print(" ");
-            for(int j=4-i;j>0;j--) System.out.print("*");
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 0; j <= i; j++) System.out.print("*");
+//            for (int j = 6 - 2 * i; j > 0; j--) System.out.print(" ");
+//            for (int j = 0; j <= i; j++) System.out.print("*");
+//            System.out.println();
+//        }
+//        for(int i=0;i<4;i++){
+//            for(int j=4-i;j>0;j--) System.out.print("*");
+//            for(int j=1;j<=2*i;j++) System.out.print(" ");
+//            for(int j=4-i;j>0;j--) System.out.print("*");
+//
+//            System.out.println();
+//        }
 
+
+// Solid Rhombus
+        for(int i=1;i<6;i++){
+            for(int j=5-i;j>0;j--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=5;j++){
+                System.out.print("*");
+            }
             System.out.println();
         }
 
