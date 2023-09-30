ea9770dab548dacc6e9b6f5f8be9d95381e163b6
AkshayO3 <akshaybhandari020@gmail.com>
2023-09-30 09:14:54 +0530

    4-6

--- src/Assign4.java
+++ src/Assign4.java
@@ -48,13 +48,30 @@ public class Assign4 {
 
 
 // 0-1 triangle
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=i;j++){
-                if((i+j)%2==0) System.out.print("1 ");
-                else System.out.print("0 ");
-            }
+//        for(int i=1;i<=5;i++){
+//            for(int j=1;j<=i;j++){
+//                if((i+j)%2==0) System.out.print("1 ");
+//                else System.out.print("0 ");
+//            }
+//            System.out.println();
+//        }
+
+
+// Butterfly Pattern
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) System.out.print("*");
+            for (int j = 6 - 2 * i; j > 0; j--) System.out.print(" ");
+            for (int j = 0; j <= i; j++) System.out.print("*");
             System.out.println();
         }
+        for(int i=0;i<4;i++){
+            for(int j=4-i;j>0;j--) System.out.print("*");
+            for(int j=1;j<=2*i;j++) System.out.print(" ");
+            for(int j=4-i;j>0;j--) System.out.print("*");
+
+            System.out.println();
+        }
+
 
     }
 }
