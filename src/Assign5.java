ac32164e72bc838fb42a1bcb6e1a3e9ce6c1a211
AkshayO3 <akshaybhandari020@gmail.com>
2023-10-03 10:38:00 +0530

    5-3

--- src/Assign5.java
+++ src/Assign5.java
@@ -6,6 +6,18 @@ public class Assign5 {
     public static boolean isEven(int n){
         return n % 2 == 0;
     }
+    public static void palindrome(int pal){
+        int rev=0,n=pal;
+        while(n>0){
+            int rem=n%10;
+            rev = rev*10+rem;
+            n/=10;
+        }
+        if(rev==pal)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not palindrome.");
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -15,5 +27,6 @@ public class Assign5 {
         int c= sc.nextInt();
         System.out.println("Average is "+avg(a,b,c));
         System.out.println(isEven(3));
+        palindrome(5765);
     }
 }
