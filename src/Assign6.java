abcd932a040e6b689ef2110974f8a0ca209edae2
AkshayO3 <akshaybhandari020@gmail.com>
2023-10-06 22:00:52 +0530

    6-3

--- src/Assign6.java
+++ src/Assign6.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
 public class Assign6 {
+    public static int binarySearch(int[] arr,int key){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(key>mid)
+                start=mid+1;
+            else if(key<mid)
+                end=mid-1;
+            else
+                return key;
+        }
+        return -1;  // If element is not found in the array
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -14,11 +28,24 @@ public class Assign6 {
 
 
 // Max element in an array
-        int[] arr={3,65,112,64,99,32,85};
-        int max=0;
-        for (int j : arr)
-            if (j > max)
-                max = j;
-        System.out.println("The maximum element of an array is "+max);
+//        int[] arr={3,65,112,64,99,32,85};
+//        int max=0;
+//        for (int j : arr)
+//            if (j > max)
+//                max = j;
+//        System.out.println("The maximum element of an array is "+max);
+
+
+// Binary Search
+        // --> Can only be inserted in a sorted array,we can reduce the size of array by half at every step.
+        int[] arr={0,1,2,3,4,5,6,7,8,9};
+        int start=0;
+        int end=arr.length-1;
+        System.out.println("Enter the element to be searched.");
+        int n = sc.nextInt();
+        if(binarySearch(arr,n)==-1)
+            System.out.println("Element is not found in the array");
+        else
+            System.out.println("Element found in the array at index "+binarySearch(arr,n));
     }
 }
