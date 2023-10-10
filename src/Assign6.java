da7426ab0e7d1fc18b2b64284d7ff13eeb22879f
AkshayO3 <akshaybhandari020@gmail.com>
2023-10-10 21:28:50 +0530

    6-12

--- src/Assign6.java
+++ src/Assign6.java
@@ -112,6 +112,13 @@ public class Assign6 {
             maxProfit = Math.max(price[i] - cp[i], maxProfit);
         System.out.println("Max profit that could be made is "+maxProfit);
     }
+    public static boolean findDuplicate(int[] arr){
+        for(int i=0;i<arr.length;i++)
+            for(int j=i+1;j<arr.length;j++)
+                if(arr[i]==arr[j])
+                    return true;
+        return false;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -164,10 +171,11 @@ public class Assign6 {
 
 
 // Printing the maximum sum in all the sub-arrays
-        int[] arr={7,1,5,3,6,4};
+        int[] arr={6,1,5,3,9,4};
 ////        maxSumSub(arr);
 //        kadaneSumMax(arr);
 //        trapWater(arr);
-        maxProfit(arr);
+//        maxProfit(arr);
+        System.out.println(findDuplicate(arr));
     }
 }
