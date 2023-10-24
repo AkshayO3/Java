25dae6b17aed5a1f402686d22d2d448743910faa
AkshayO3 <akshaybhandari020@gmail.com>
2023-10-24 09:25:57 +0530

    Getter-Setter

--- OOPs/src/AccessModifiers.java
+++ OOPs/src/AccessModifiers.java
@@ -4,16 +4,27 @@
  * default          | Y     | Y       | N        | N
  * private          | Y     | N       | N        | N
  */
+
+// Private values can be read and modified by using getter and setter functions outside the class.
 class Bank{
-    private int password;
+    private int password=98765434;
     int accNo;
     protected String Name;
     public int Balance;
+    public int getPassword(){
+        return this.password;
+    }
+    public int setPassword(int pass){
+        this.password=pass;
+        return this.password;
+    }
 }
 public class AccessModifiers {
     public static void main(String[] args) {
         Bank b1 = new Bank();
         System.out.println(b1.accNo);
-        System.out.println(b1.password);    //Returns error as the password is private
+//        System.out.println(b1.password);    //Returns error as the password is private
+        System.out.println(b1.getPassword());
+        System.out.println(b1.setPassword(35428967));
     }
 }
